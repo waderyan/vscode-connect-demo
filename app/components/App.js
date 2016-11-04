46977f4cc5ebcfeb617bcf154fe4cbdb3c1cd4f9
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-11-04 11:15:07 -0700

    init

---
+++ app/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import IconButton from 'material-ui/lib/icon-button';
+import StarIcon from 'material-ui/lib/svg-icons/toggle/star';
+
+
+
+import Viewport from './Viewport';
+
+
+class App extends React.Component {
+    
+    constructor() {
+        super();
+        this.styles ={
+            backgroundColor: '#000'
+        };
+    }
+
+    getChildContext(){
+      return {
+        muiTheme: getMuiTheme()
+      };
+    }
+
+    render() {
+        return (
+            <div>
+                <AppBar 
+                    style={this.styles}
+                    iconElementLeft={<IconButton><StarIcon /></IconButton>}
+                    title="Todos" 
+                />
+                <Viewport />
+            </div>
+        );
+    }
+}
+
+
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
+
+
+export default App;
---
+++ app/components/Todos.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import IconButton from 'material-ui/lib/icon-button';
+import ChevronRightIcon from 'material-ui/lib/svg-icons/navigation/chevron-right';
+
+import TodosStore from './TodosStore';
+
+
+class Todos extends React.Component {
+  
+  constructor() {
+      super();
+      this.styles = {
+        float: 'left',
+        width: '50%',
+        margin: '3%'
+      };
+      this.state = { todos: [] };
+  }
+
+  componentDidMount() {
+    TodosStore.getAll().then((data) => {
+      console.log('get all', data);
+      this.setState({
+        todos: data.todos
+      });
+    });
+    
+    TodosStore.subscribe((action) => {
+      this.setState({
+        todos: action.todos
+      });
+    });
+  }
+  
+  handleClick(note) {
+    TodosStore.remove(note);
+  }
+  
+  create(todo) {
+    return (<ListItem 
+              onMouseDown={this.handleClick.bind(null, todo)} 
+              key={todo.id}
+              leftIcon={<ChevronRightIcon />}
+              primaryText={todo.text}
+              secondaryText={todo.timestamp}>
+           </ListItem>
+    );
+  }
+
+  render() {
+    const todos = this.state.todos.map(this.create.bind(this));
+    return (
+      <div>
+        <List style={this.styles}>
+          {todos}
+        </List>
+        <select>
+            <option value="1">Q1. Jan - Mar</option>
+            <option value="2">Q2. Apr - Jun</option>
+            <option value="3">Q3. Jul - Sep</option>
+            <option value="4">Q4. Oct - Dec</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default Todos;
